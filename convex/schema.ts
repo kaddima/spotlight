e74eb00b66bce07e07d6055d3c0561e888f88aeb
kadima <williamsvgw@gmail.com>
2025-05-06 00:01:04 +0100

    added more convex schemas

--- convex/schema.ts
+++ convex/schema.ts
@@ -37,4 +37,28 @@ export default defineSchema({
 		postId: v.id("posts"),
 		content: v.string(),
 	}).index("by_post", ["postId"]),
+
+	follows: defineTable({
+		followerId: v.id("users"),
+		followingId: v.id("users"),
+	})
+		.index("by follower", ["followerId"])
+		.index("by_following", ["followingId"])
+		.index("by_both", ["followerId", "followingId"]),
+
+	Notifications: defineTable({
+		receiverId: v.id("users"),
+		senderId: v.id("users"),
+		type: v.union(v.literal("like"), v.literal("comment"), v.literal("follow")),
+		postId: v.optional(v.id("posts")),
+		commentId: v.optional(v.id("comments")),
+	}).index("by_receiver", ["receiverId"]),
+
+	bookmarks: defineTable({
+		userId: v.id("users"),
+		postId: v.id("posts"),
+	})
+		.index("by_user", ["userId"])
+		.index("by_post", ["postId"])
+		.index("by_user_and_post", ["userId", "postId"]),
 });
